21b0f291738b9e2b3d9a206eccd717156ddb6faf
solankirishav21 <btech10645.20@bitmesra.ac.in>
2021-10-28 19:01:01 +0530

    added javascript

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Connected")
+// console.log("Connected")
+let randomNo = Math.floor((Math.random()*6)+1);
+let randomNo2 = Math.floor((Math.random()*6)+1);
+// console.log(randomNo1);
+
+let diceImage = "images/dice" + randomNo + ".png";
+let diceImage2 = "images/dice" + randomNo2 + ".png";
+// console.log(diceImage);
+
+let img1 = document.querySelectorAll("img")[0];
+let img2 = document.querySelectorAll("img")[1];
+
+img1.setAttribute("src", diceImage);
+img2.setAttribute("src",diceImage2);
+
+if(randomNo > randomNo2){
+    document.querySelector("h1").innerText = "Player 1 Wins!";
+}
+else if(randomNo === randomNo2){
+    document.querySelector("h1").innerText = "Why not give it a other try?";
+    document.querySelector("h1").style.fontSize="5.5rem";
+}
+else{
+    document.querySelector("h1").innerText = "Player 2 Wins!"
+}
+
+
